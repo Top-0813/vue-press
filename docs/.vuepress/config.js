1bc53428651db661462878d25b30d5b30971614c
文艺小码农 <854261062@qq.com>
2020-05-06 00:12:27 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    base: '/',
+    title: 'Top vlog',
+    description: 'Just playing around',
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+      repo: 'https://github.com/Top-0813',
+      // 自定义仓库链接文字。
+      repoLabel: 'My GitHub',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+        ]
+  }
+}
